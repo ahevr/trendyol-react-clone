37439b22709d77541d960b5af99ac4c708cb3fe9
ahevr <ahmethusreversen@hotmail.com>
2023-02-14 02:41:53 +0300

    router-eklendi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import Header from 'components/Header'
-import Menu from 'components/Menu'
-import Story from 'components/Story'
-import TopHeader from 'components/TopHeader'
+import React from 'react';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import React from 'react'
-import Banner from 'components/Banner';
-import PopulerProduct from 'components/PopulerProduct';
-import CardTwo from 'components/CardTwo';
-import BestPrice from 'components/BestPrice';
+import {Routes,Route,Link,NavLink} from "react-router-dom";
+// components
+
+import About from 'pages/About';
+
+import Home from 'pages/Home';
+
 
 const App = () => {
   return (
+    
     <div className='container mx-auto'>
-      <TopHeader/>
-      <Header/>
-      <Menu/>
-      <hr className='h-1 mt-5 ' />
-      <Story />
-      <Banner />
-      <PopulerProduct />
-      <CardTwo />
-      <BestPrice />
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/About' element= {<About/>} />
+      </Routes>
+      
+   
     </div>
+    
   )
 }
 
